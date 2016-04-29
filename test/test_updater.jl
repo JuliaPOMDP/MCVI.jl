61952ab8c57665bc7d3996d48b19f9591a8a0888
rejuvyesh <mail@rejuvyesh.com>
2016-04-29 10:49:20 -0700

    function needs hyperparam argument

--- test/test_updater.jl
+++ test/test_updater.jl
@@ -2,18 +2,18 @@ function test_backup()
     p = LightDark1D()
     policy = MCVIPolicy(p)
     MCVI.initialize_updater!(policy)
-    b0 = MCVI.initial_belief(p)
+    b0 = MCVI.initial_belief(p, 500)
     # s0 = initial_state(p)
     # sim = MCVISimulator(MersenneTwister(420), s0, 1)
 
     sim = MCVISimulator()
 
-    n, _ = MCVI.backup(b0, policy, sim, p)
+    n, _ = MCVI.backup(b0, policy, sim, p, 500)
     MCVI.addnode!(policy.updater, n)
     policy.updater.root = n
 
     v1 = MCVI.evaluate(policy, sim, p)
-    n, _ = MCVI.backup(b0, policy, sim, p)
+    n, _ = MCVI.backup(b0, policy, sim, p, 500)
     MCVI.addnode!(policy.updater, n)
     policy.updater.root = n
 
